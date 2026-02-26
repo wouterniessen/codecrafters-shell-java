e61473a49750fc07367852f1bd8510b15f654c6b
wniessen <wouter.niessen@outlook.com>
2026-02-26 23:07:43 +0100

    codecrafters submit [skip ci]

--- src/main/java/utils/Type.java
+++ src/main/java/utils/Type.java
@@ -5,7 +5,7 @@ public class Type {
     private static List<String> builtinCommands = Arrays.asList("echo", "exit", "type");
 
     protected static void getType(String command) {
-        if (builtinCommands.stream(builtinCommands).anyMatch(command::equals)) {
+        if (builtinCommands.stream().anyMatch(command::equals)) {
             System.out.println(String.format("%s is a shell builtin", command));
         } else {
             System.out.println(String.format("%s: not found", command));
