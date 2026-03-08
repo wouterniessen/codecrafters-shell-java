8323611df56c6684483cbc232a97894a3f5b0844
wniessen <wouter.niessen@outlook.com>
2026-03-08 21:41:31 +0100

    codecrafters submit [skip ci]

--- src/main/java/utils/Parser.java
+++ src/main/java/utils/Parser.java
@@ -6,27 +6,52 @@ import java.util.List;
 public class Parser {
 
     public static List<String> parseArgs(String input) {
+
         List<String> args = new ArrayList<>();
         StringBuilder current = new StringBuilder();
 
         boolean inSingleQuote = false;
+        boolean inDoubleQuote = false;
+        boolean escaping = false;
 
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
 
-            if (c == '\'') {
+            // handle escape
+            if (escaping) {
+                current.append(c);
+                escaping = false;
+                continue;
+            }
+
+            // escape start
+            if (c == '\\' && !inSingleQuote) {
+                escaping = true;
+                continue;
+            }
+
+            // single quote toggle
+            if (c == '\'' && !inDoubleQuote) {
                 inSingleQuote = !inSingleQuote;
                 continue;
             }
 
-            if (Character.isWhitespace(c) && !inSingleQuote) {
+            // double quote toggle
+            if (c == '"' && !inSingleQuote) {
+                inDoubleQuote = !inDoubleQuote;
+                continue;
+            }
+
+            // argument separator
+            if ((c == ' ' || c == '\t') && !inSingleQuote && !inDoubleQuote) {
                 if (current.length() > 0) {
                     args.add(current.toString());
                     current.setLength(0);
                 }
-            } else {
-                current.append(c);
+                continue;
             }
+
+            current.append(c);
         }
 
         if (current.length() > 0) {
